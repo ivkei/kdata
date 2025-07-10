62e3bea4cbc0fbe96d3a21f71efd1f4dab3b8db9
debugkei <ivankei0009@gmail.com>
2025-07-10 16:41:08 -0700

    Renamed src to include

--- tests/darray.cpp
+++ tests/darray.cpp
@@ -1,5 +1,4 @@
-#include"../src/darray.h"
-//TODO(kei): Rename src to include
+#include"../include/kdata/darray.h"
 
 int main(){
   //TODO(kei): Write a test, and wrap it into a timer than then is compared to same std::vector test's timer
